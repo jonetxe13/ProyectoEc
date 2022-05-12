262bef8196559e735e5389141f2fc86c1e6d2f64
jonetxe13 <jonetxeba.je10@gmail.com>
2022-05-12 12:47:44 +0200

    funciones de ataque metidas

--- ProyectoEc/source/sprites.c
+++ ProyectoEc/source/sprites.c
@@ -15,6 +15,7 @@ u16* gfxsonic;
 u16* gfxPouAsesino;
 u16* gfxSonic;
 u16* gfxGoku;
+u16* gfxAtaque;
 
 /* Reservar memoria para cada sprite que se quiera mostrar en pantalla.*/
 void memoriaReserba()
@@ -24,6 +25,7 @@ void memoriaReserba()
 	gfxPouAsesino=oamAllocateGfx(&oamMain, SpriteSize_32x32, SpriteColorFormat_256Color);
 	gfxSonic=oamAllocateGfx(&oamMain, SpriteSize_32x32, SpriteColorFormat_256Color);
 	gfxGoku=oamAllocateGfx(&oamMain, SpriteSize_32x32, SpriteColorFormat_256Color);
+	gfxAtaque=oamAllocateGfx(&oamMain, SpriteSize_32x32, SpriteColorFormat_256Color);
 }
 
 /* A cada uno de los 256 valores que puede tomar un pixel en la PALETA PRINCIPAL
@@ -101,7 +103,7 @@ u8 sonic[256] =
 	0,0,0,0,0,6,1,1,
         3,1,1,6,3,1,1,6,	//	0,0,0,0,0,6,1,1, 3,1,1,6,3,1,1,6,
 };
-u8 ataque[1024] =
+u8 Ataque[1024] =
 {
 0 ,	0 ,	0 ,	0 ,	0 ,	0 ,	0 ,	0 , 0 ,	0 ,	0 ,	0 ,	0 ,	0 ,	0 ,	0 , 0 ,	0 ,	0 ,	0 ,	0 ,	0 ,	0 ,	0 , 0 ,	0 ,	0 ,	0 ,	0 ,	0 ,	0 ,	0 ,
 0 ,	0 ,	0 ,	0 ,	0 ,	0 ,	0 ,	0 , 0 ,	0 ,	0 ,	0 ,	0 ,	0 ,	0 ,	0 , 0 ,	0 ,	0 ,	0 ,	0 ,	0 ,	0 ,	0 , 0 ,	0 ,	0 ,	0 ,	0 ,	0 ,	0 ,	0 ,
@@ -315,6 +317,10 @@ int i;
 	{	
 		gfxGoku[i] = Goku[i*2] | (Goku[(i*2)+1]<<8);				
 	}
+	for(i = 0; i < 32 * 32 / 2; i++) 
+	{	
+		gfxAtaque[i] = Ataque[i*2] | (Ataque[(i*2)+1]<<8);				
+	}
 }
 
 /* Esta función dibuja un rombo en la posición x-y de pantalla. A cada rombo que se quiera mostrar en pantalla se le debe asignar un indice distinto, un valor entre 0 y 126. */
@@ -481,6 +487,49 @@ oamSet(&oamMain, //main graphics engine context
 		SpriteSize_32x32,     
 		SpriteColorFormat_256Color, 
 		gfxGoku,//+16*16/2,                  //pointer to the loaded graphics
+		-1,                  //sprite rotation data  
+		false,               //double the size when rotating?
+		true,			//hide the sprite?
+		false, false, //vflip, hflip
+		false	//apply mosaic
+		); 
+oamUpdate(&oamMain); 
+
+}
+
+void MostrarAtaque(int indice, int x, int y)
+{ 
+ 
+oamSet(&oamMain, //main graphics engine context
+		indice,           //oam index (0 to 127)  
+		x, y,   //x and y pixle location of the sprite
+		0,                    //priority, lower renders last (on top)
+		0,					  //this is the palette index if multiple palettes or the alpha value if bmp sprite	
+		SpriteSize_32x32,     
+		SpriteColorFormat_256Color, 
+		gfxAtaque,//+16*16/2,                  //pointer to the loaded graphics
+		-1,                  //sprite rotation data  
+		false,               //double the size when rotating?
+		false,			//hide the sprite?
+		false, false, //vflip, hflip
+		false	//apply mosaic
+		); 
+
+	  
+oamUpdate(&oamMain);  
+}
+
+void BorrarAtaque(int indice, int x, int y)
+{
+
+oamSet(&oamMain, //main graphics engine context
+		indice,           //oam index (0 to 127)  
+		x, y,   //x and y pixle location of the sprite
+		0,                    //priority, lower renders last (on top)
+		0,					  //this is the palette index if multiple palettes or the alpha value if bmp sprite	
+		SpriteSize_32x32,     
+		SpriteColorFormat_256Color, 
+		gfxAtaque,//+16*16/2,                  //pointer to the loaded graphics
 		-1,                  //sprite rotation data  
 		false,               //double the size when rotating?
 		true,			//hide the sprite?
