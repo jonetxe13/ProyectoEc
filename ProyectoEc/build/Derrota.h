2a7f3f96f561594b8cbe888c1c64b4ff07ecf559
jonetxe13 <jonetxeba.je10@gmail.com>
2022-05-12 10:38:20 +0200

    borrar v para matxin

--- ProyectoEc/build/Derrota.h
+++ ProyectoEc/build/Derrota.h
@@ -8,7 +8,7 @@
 //	+ bitmap not compressed
 //	Total size: 97792 = 97792
 //
-//	Time-stamp: 2022-05-12, 11:31:01
+//	Time-stamp: 2022-05-12, 10:37:50
 //	Exported by Cearn's GBA Image Transmogrifier, v0.8.16
 //	( http://www.coranac.com/projects/#grit )
 //
