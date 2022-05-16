1bc102249c7188ee360934ce60333cf78b9319f9
jonetxe13 <jonetxeba.je10@gmail.com>
2022-05-16 12:02:35 +0200

    ataque bieennnn

--- ProyectoEc/source/rutinasAtencion.c
+++ ProyectoEc/source/rutinasAtencion.c
@@ -17,8 +17,10 @@ int seg3;
 int player = 0;
 int HP1 = vida;
 int HP2 = vida;
-int posx= 70;
-int posy= 130;
+int posx= 50;
+int posy= 155;
+int pos2x= 180;
+int pos2y= 105;
 
 void RutAtencionTeclado (){
 
@@ -29,20 +31,6 @@ void RutAtencionTeclado (){
 		}
 	}
 	else if (ESTADO == PELEA){
-		if(player == 0){
-			int Nataques;	
-			if(TeclaPulsada() == A){
-				Nataques = rand()%3;
-				HP2 -= 10*Nataques;
-				player = 1;
-				iprintf("\x1b[13;2HHa atacado %d",Nataques," nº de veces");
-			}
-			else if(TeclaPulsada() == B){
-				HP2 -= 10;
-				player = 1;
-				iprintf("\x1b[2J");
-			}
-		}
 	}
 	else if(ESTADO == FIN){
 		player = 0;
@@ -59,38 +47,51 @@ void RutAtencionTeclado (){
 	}
 }
 
+int contar = 0;
+
 void RutAtencionTempo()
 {
 	static int tick=0;
 	static int seg=0;
 	static int tickAnimaciones = 0;
+	static int tickAnimaciones2 = 0;
 
 	if (ESTADO==PELEA)
 	{
 		tick++; 
 		tickAnimaciones++;
+		tickAnimaciones2++;
 		//animation for MostrarAtaque in diagonal
-
 		if(player == 0){
-			if(tickAnimaciones < 10){
-				MostrarAtaque(12,posx,posy);
-				posx += 12;
-				posy -= 12;
+				int Nataques;	
+				if(TeclaPulsada() == A && contar == 0){
+					contar = 1;
+					tickAnimaciones = 0;
+					Nataques = rand()%3;
+					HP2 -= 10*Nataques;
+					iprintf("\x1b[13;2HHa atacado %d",Nataques," nº de veces");
 				}
-			else{
-				tickAnimaciones == 0;
-				posx = 70;
-				posy = 130;
-			}
+				else if(TeclaPulsada() == B && contar == 0){
+					contar = 1;
+					tickAnimaciones = 0;
+					HP2 -= 10;
+					iprintf("\x1b[2J");
+				}
+				if(tickAnimaciones < 10 && contar == 1){
+					MostrarAtaque(12,posx,posy);
+					posx += 16;
+					posy -= 6;
+				}
+				else if(tickAnimaciones >= 10 && contar == 1){
+					tickAnimaciones = 0;
+					contar = 0;
+					player = 1;
+					posx = 50;
+					posy = 155;
+				}
 		}
-		if (tick==5)
-		{
-			tick=0;
-
-			if(player == 1){
-
-				seg++;
-				if(seg == 2){
+		else if(player == 1){
+			if(tickAnimaciones == 10){
 					int aleatorio = rand()%2;
 					if(aleatorio == 0){
 						HP1 -= 5; 
@@ -101,11 +102,29 @@ void RutAtencionTempo()
 						player = 0;
 						iprintf("\x1b[2J");
 					}
-					seg = 0;
+					tickAnimaciones = 0;
+					contar = 0;
 				}
-			}
+				if(tickAnimaciones2 < 10){
+					MostrarAtaque(12,pos2x,pos2y);
+					pos2x -= 16;
+					pos2y += 6;
+				}
+				else if(tickAnimaciones2 >= 10){
+					tickAnimaciones2 = 0;
+					contar = 0;
+					player = 1;
+					pos2x = 180;
+					pos2y = 105;
+				}
+		}
+
+		if (tick==5)
+		{
+			tick=0;
+
 			seg3++;
-			if (seg3==50)
+			if (seg3==150)
 			{
 				seg3=0;
 				ESTADO=FIN;
